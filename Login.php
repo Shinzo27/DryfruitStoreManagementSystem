d8616a55413067c27f7bb8fff89a99a39493ef84
Shinzo27 <20bmiit031@gmail.com>
2022-08-15 10:54:21 +0530

    15-08

--- Login.php
+++ Login.php
@@ -120,6 +120,18 @@
       </div>
     </div>
   </div>
+  
+  <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/js/lightgallery.min.js"></script>
+
+   <!-- custom js file link  -->
+   <script src="js/script.js"></script>
+
+   <script>
+      lightGallery(document.querySelector('.gallery .gallery-container'));
+   </script>
+   
 </body>
 
 </html>
