89dde6aa2bcca3f616688b88cae8649dc0a89738
Shinzo27 <20bmiit031@gmail.com>
2022-09-21 22:43:13 +0530

    21-09

---
+++ admin/datacon.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "dsms"; 
+
+    $conn = mysqli_connect($servername,$username,$password,$database);
+
+?>
